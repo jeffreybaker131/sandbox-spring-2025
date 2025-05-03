8e0a1877b3c9ef9da31c67144afb399171e770dd
jeffreybaker131 <JEF2193187@maricopa.edu>
2025-05-02 17:29:52 -0700

    Tree

---
+++ src/main/java/org/example/sandbox/trees/ElementNotFoundException.java
@@ -0,0 +1,29 @@
+package org.example.sandbox.trees;
+
+public class ElementNotFoundException extends Exception {
+
+    public ElementNotFoundException() {
+
+        super();
+
+    }
+
+    public ElementNotFoundException(String message) {
+
+        super(message);
+
+    }
+
+    public ElementNotFoundException(String message, Throwable t) {
+
+        super(message, t);
+
+    }
+
+    public ElementNotFoundException(Throwable t) {
+
+        super(t);
+
+    }
+
+}
